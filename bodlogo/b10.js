6ecf69df6d4af48d0ca7a03e1b4fe57bb5222547
baatarnyam <bbaatarnya@gmail.com>
2023-11-06 21:22:06 +0800

    10r bodlogo dutuu

--- bodlogo/b10.js
+++ bodlogo/b10.js
@@ -1,3 +1,9 @@
-const a = 2
-const b = 3
-const c = 5
+const a = 8, b = 5, c = 7
+p = (a+b+c)/2
+s = (p*(p-a)*(p-b)*(p-c))
+if (s <= 0){
+    console.log("gurvaljin bish baina")
+}
+else {
+    console.log(s)
+}
