5abf0cb496117f40b571749f198ee1c89d7f3b3d
bn.git <bbaatarnya@gmail.com>
2023-11-07 09:32:21 +0800

    10t uurchlult oruulav

--- bodlogo/b10.js
+++ bodlogo/b10.js
@@ -1,9 +1,5 @@
-const a = 8, b = 5, c = 7
+const a = 8, b = 5, c = 8
 p = (a+b+c)/2
 s = Math.sqrt(p*(p-a)*(p-b)*(p-c))
-if (s <= 0){
-    console.log("gurvaljin bish baina")
-}
-else {
-    console.log(s)
-}
+console.log(s)
+
