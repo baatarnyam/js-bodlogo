2621609d98f914c08d8d1907a61edc547dbb25c1
bn.git <bbaatarnya@gmail.com>
2023-11-09 10:28:37 +0800

    turshilt

---
+++ home/b2.js
@@ -0,0 +1,18 @@
+
+const number = 1002
+const left = number % 10 
+
+    switch (left){ 
+        case 1 || 6 : console.log("1 deh udur yavahgui");
+        break;
+        case 2 || 7: console.log("2 deh udur yavahgui");
+        break;
+        case 3 || 8: console.log("3 deh udur yavahgui");
+        break;
+        case 4 || 9: console.log("4 deh udur yavahgui");
+        break;
+
+        default : console.log("5 deh udur yavahgui");
+        break;
+
+    }
